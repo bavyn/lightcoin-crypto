bec192c62b8789cf6deb21a84b95cbd68cb02a94
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-16 13:08:05 -0700

    moved commit to transaction class

--- index.js
+++ index.js
@@ -12,18 +12,20 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
-
 class Deposit extends Transaction {
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 }
 
 class Withdrawal extends Transaction {
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 }
 
@@ -32,17 +34,13 @@ class Withdrawal extends Transaction {
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
 const myAccount = new Account("snow-patrol");
+console.log('Starting balance: ', myAccount.balance);
 
-t1 = new Withdrawal(50.25, myAccount);
+const t1 = new Deposit(500, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('New balance: ', myAccount.balance);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(200, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3: ', t3);
-
-console.log('Balance:', myAccount.balance);
+console.log('Ending balance:', myAccount.balance);
